0818a52037dadfd62c359b347125a2f825dad902
DESKTOP-RUS50PJ\Nguye <phuong1505hn@gmail.com>
2020-11-07 21:56:36 +0700

    update bản xịn, đã chạy ngon, chưa xử lý được hình ảnh, sửa path ảnh ở f3

--- P_DiemDanh/TestOnThis/Attendance_GUI.py
+++ P_DiemDanh/TestOnThis/Attendance_GUI.py
@@ -36,6 +36,9 @@ def resetTT():
     txtHoTen.delete(0, 'end')
     txtLop.delete(0, 'end')
     txtNgaySinh.delete(0, 'end')
+    lblBuoc1.configure(text = "Bước 1:", fg= _primary)
+    lblBuoc2.configure(text = "Bước 2:", fg= _primary)
+    lblBuoc3.configure(text = "ĐIỂM DANH", fg= _primary)
 
 # để dữ liệu nhập vào ô nhập tên là viết thường
 def getName():
@@ -43,7 +46,7 @@ def getName():
     translator = str.maketrans('', '', string.punctuation)
     name = name.translate(translator)
     name = unidecode(name)
-    name = name.replace(" ", "-")
+    name = name.replace(" ", ".")
     return name
 
 #kiểm tra xem kiểu dữ liệu nhập vào có phải số không
@@ -89,7 +92,7 @@ def TakeImmages():
                 for(x, y, w, h) in faces:
                     cv2.rectangle(img, (x, y), (x+w, y + h), (255, 0, 0), 2)
                     sampleNum = sampleNum+1
-                    filenameAut = "TrainingImages/"+ Id + "-" + name + '-' + str(sampleNum) + ".png"
+                    filenameAut = "TrainingImages/"+ name + "." + Id + '.' + str(sampleNum) + ".png"
                     cv2.imwrite(filenameAut , gray[y:y+h, x:x+w])
                     cv2.imshow('Get Image data train', img)
                 if cv2.waitKey(100) & 0xFF == ord('q'):
@@ -115,21 +118,39 @@ def TakeImmages():
 
 
 def TrainImages():
-    # Path for face image database
-    
+    # Path for face image database   
+    path = "TrainingImages/"
     recognizer = cv2.face.LBPHFaceRecognizer_create()
     detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
     # function to get the images and label data    
     print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
-    faces,ids = getImagesAndLabels(detector)
+    faces,ids = getImagesAndLabels(detector, path)
     recognizer.train(faces, np.array(ids))
     # Save the model into trainer/trainer.yml
     recognizer.write('TrainingImageLabel/Trainner.yml') 
     # Print the numer of faces trained and end program
     print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
+    # res = "Image Trainned"+",".join(str(f) for f in ids)
+    res = "Xử lý dữ liệu thành công"
+    lblBuoc2.configure(text=res, fg = _green)
 
-def getImagesAndLabels(detector):
-        path = "TrainingImages/"
+# def getImagesAndLabels(detector):
+#         path = "TrainingImages/"
+#         imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
+#         faceSamples=[]
+#         ids = []
+#         for imagePath in imagePaths:
+#             PIL_image = Image.open(imagePath)
+#             PIL_img= PIL_image.convert('L') 
+#             img_numpy = np.array(PIL_img,'uint8')
+#             id = int(os.path.split(imagePath)[-1].split(".")[1])
+#             faces = detector.detectMultiScale(img_numpy)
+#             for (x,y,w,h) in faces:
+#                 faceSamples.append(img_numpy[y:y+h,x:x+w])
+#                 ids.append(id)
+#         return faceSamples,ids
+
+def getImagesAndLabels(detector, path):
         imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
         faceSamples=[]
         ids = []
@@ -137,7 +158,7 @@ def getImagesAndLabels(detector):
             PIL_image = Image.open(imagePath)
             PIL_img= PIL_image.convert('L') 
             img_numpy = np.array(PIL_img,'uint8')
-            id = int(os.path.split(imagePath)[-1].split("-")[0])
+            id = int(os.path.split(imagePath)[-1].split(".")[1])
             faces = detector.detectMultiScale(img_numpy)
             for (x,y,w,h) in faces:
                 faceSamples.append(img_numpy[y:y+h,x:x+w])
@@ -145,57 +166,98 @@ def getImagesAndLabels(detector):
         return faceSamples,ids
 
 
-
 def TrackImages():
-    recognizer = cv2.face_LBPHFaceRecognizer.create()
-    recognizer.read("TrainingImageLabel\Trainner.yml")
+    recognizer = cv2.face.LBPHFaceRecognizer_create() 
+    recognizer.read("TrainingImageLabel"+os.sep+"Trainner.yml")
     harcascadePath = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
     faceCascade = cv2.CascadeClassifier(harcascadePath)
-    df = pd.read_csv("StudentDetails\studentDetails.csv")
-    #cam = cv2.FONT_HERSHEY_SIMPLEX  
-    cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    df = pd.read_csv("StudentDetails"+os.sep+"studentDetails.csv")
+    cam = cv2.VideoCapture(0)
+    font = cv2.FONT_HERSHEY_SIMPLEX
     col_names = ['Id', 'Name', 'Date', 'Time']
     attendance = pd.DataFrame(columns=col_names)
+    ##
+    col_info = ['Id', 'Name', 'Dob', 'Lop']
+    info = pd.DataFrame(columns=col_info)
+
     while True:
-        ret, img = cam.read()
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+        ret, im = cam.read()
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(gray, 1.2, 5)
         for(x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x+w, y + h), (255, 0, 0), 2)
+            cv2.rectangle(im, (x, y), (x+w, y+h), (225, 0, 0), 2)
             Id, conf = recognizer.predict(gray[y:y+h, x:x+w])
+
             if(conf < 50):
                 ts = time.time()
                 date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
                 timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
-                aa = df.loc[df['Id'] == Id] ['Name'].values
-                tt =str(Id)+"-"+ aa
+                aa = df.loc[df['Id'] == Id]['Name'].values[0]
+                tt = str(Id)+"-"+aa
                 attendance.loc[len(attendance)] = [Id, aa, date, timeStamp]
+                ###
+                infoName = df.loc[df['Id'] == Id]['Name'].values[0]
+                inforLop = df.loc[df['Id'] == Id]['Lop'].values[0]
+                inforDob = df.loc[df['Id'] == Id]['Dob'].values[0]
+                info.loc[len(attendance)] = [Id, infoName, inforDob, inforLop]
+                   
+                resInfor = "Sinh viên: " + infoName + "\n MSSV: " + str(Id) + "\n Lớp: " + inforLop
+                lblMessage.configure(text = resInfor)
+
+
             else:
                 Id = 'Unknown'
+                resInfor = "Sinh viên: " + str(Id) + "\n MSSV: " + str(Id) + "\n Lớp: " + str(Id)
+                lblMessage.configure(text = resInfor)             
+                res= "ĐIỂM DANH KHÔNG THÀNH CÔNG"
+                lblThongBao.configure(text = res, fg= _button)  
                 tt = str(Id)
-                if(conf>75):
-                    noOfFile = len(os.listdir("ImagesUnknown")) + 1
-                    cv2.imwrite("ImagesUnknown\Image" + str(noOfFile) + ".png", img[y:y+h, x:x+w])
-                cv2.putText(img, str(tt), (x,y+h), font, 1, (255,255,255), 2)
-        attendance = attendance.drop_duplicates(subset = ['Id'], keep = 'first')
-        cv2.imshow('Diem danh', img)
-        if(cv2.waitKey(1) == ord('q')):
+                if(conf > 75):
+                    noOfFile = len(os.listdir("ImagesUnknown"))+1
+                    cv2.imwrite("ImagesUnknown"+os.sep+"Image"+str(noOfFile) +
+                                ".png", im[y:y+h, x:x+w])
+
+            cv2.putText(im, str(tt), (x, y+h), font, 1, (255, 255, 255), 2)
+
+        attendance = attendance.drop_duplicates(subset=['Id'], keep='first')
+        info = info.drop_duplicates(subset=['Id'], keep='first')
+
+        cv2.imshow('Diem danh', im)
+        if (cv2.waitKey(1) == ord('q')):
             break
         ts = time.time()
         date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
-        timeStamp =datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
-        Hour, Minute, Second  = timeStamp.split(":")
-        fileName = "Attendance\Attendance_" + date+ "_" + Hour + "-" + Minute + "-" + Second + ".csv"
+        timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+        Hour, Minute, Second = timeStamp.split(":")
+        fileName = "Attendance"+os.sep+"Attendance_"+date+"_"+Hour+".csv"
+        attendance.to_csv(fileName, index=False)
+        res= "ĐIỂM DANH THÀNH CÔNG"
+        lblThongBao.configure(text = res, fg = _green)
+ 
+    cam.release()
+    cv2.destroyAllWindows() 
+    resTime = Hour + ":" + Minute + ", " + date
+    lblSetThoiGian.configure(text = resTime)
+    naviDiemDanh()
 
-        print(fileName)
 
-        attendance.to_csv(fileName, index = True)
-        cam.release()
-        res= attendance
-        lblBuoc3.configure(text = res)
+#chuyển sang màn thêm sinh thông tin sv
+def naviThemTT():
+    raise_frame(f2)
+
+#chuyển sang màn điểm sanh
+def naviDiemDanh():
+    raise_frame(f3)
 
 
+def updateImg():
+    fileUnkonwn = "hoa.png"
+    imgUnknown = PhotoImage(file=fileUnkonwn)
+    imgImage.itemconfig(imgOnCanvas, image=imgUnknown)
 
+################################################################################################
+################################################################################################
+################################################################################################
 
 root = tk.Tk()
 root.title('Nhận diện khuôn mặt')
@@ -216,23 +278,34 @@ for frame in (f1, f2, f3):
     frame.grid(row=0, column=0, sticky='news')
 
 
-############################################# F1
+
+
+################################################################################################
+################################################################################################
+################################################################################################
+############################################# F1: trang chủ
+
 lblTitle = Label(f1, text='Hệ thống điểm danh bằng nhận diện khuôn mặt', font=(
     "Segoe UI", 30, "bold"), bg=_primary, fg=_yellow).pack(padx=170, pady=70)
 
 
-btnDiemDanh = Canvas(f1, bg=_white, width=217, height=217)
+btnDiemDanh = Button(f1, bg = _white, width  = 217 , height = 217, command=TrackImages)
 btnDiemDanh.place(x=387, y=230)
-imgDiemDanh = PhotoImage(file="btn_diem_danh.png")
-btnDiemDanh.create_image(110, 110, anchor=CENTER, image=imgDiemDanh)
+miDiemDanh = PhotoImage(file = "btn_diem_danh.png")
+tmiDiemDanh = miDiemDanh.subsample(1,1)
+btnDiemDanh.config(image = tmiDiemDanh, compound = RIGHT)
 
 
-btnThemTT = Canvas(f1, bg=_white, width=217, height=217)
+btnThemTT = Button(f1, bg=_white, width=217, height=217, command=lambda:raise_frame(f2))
 btnThemTT.place(x=677, y=230)
-imgThemTT = PhotoImage(file="btn_them_tt.png")
-btnThemTT.create_image(110, 110, anchor=CENTER, image=imgThemTT)
+miThemTT = PhotoImage(file="btn_them_tt.png")
+tmiThemTT = miThemTT.subsample(1,1)
+btnThemTT.config(image = tmiThemTT, compound = RIGHT)
 
 
+################################################################################################
+################################################################################################
+################################################################################################
 ############################################### F2: Thêm thông tin sinh viên
 
 
@@ -273,7 +346,7 @@ lblLop = Label(f2, text="Lớp", font=("Segoe UI", 15),
 txtLop = Entry(f2, width=40, font=("Segoe UI", 15),relief =SUNKEN, bg = _lightEntry)
 txtLop.place(x=300, y=518)
 
-#####
+################################################################################################
 btnReset = tk.Button(f2, text="Reset thông tin", bg=_white, relief = FLAT,
                  fg=_primary, font=("Segoe UI", 12), command = resetTT).place(x=300, y=555)
 
@@ -294,21 +367,20 @@ btnXuLyHinhAnh = Button(f2, text="Xử lý hình ảnh", fg=_white,  relief=FLAT
                 "Segoe UI", 15), bg=_button, width=30, command = TrainImages).place(x=809, y=380)
 
 
-lblBuoc3 = Label(f2, text="Bước 3: ", font=(
+lblBuoc3 = Label(f2, text="ĐIỂM DANH", font=(
                 "Segoe UI", 12), bg=_white, fg=_primary)
 lblBuoc3.place(x=809, y=448)
-btnLuuHinhAnh = Button(f2, text="Lưu dữ liệu hình ảnh", fg=_white,  relief=FLAT,font=(
+btnLuuHinhAnh = Button(f2, text="Test", fg=_white,  relief=FLAT,font=(
                 "Segoe UI", 15), bg=_button, width=30, command = TrackImages).place(x=809, y=476)
 
 
-btnLuu = Button(f2, text="LƯU", fg=_white, bg=_lightBlue,
-                font=("Segoe UI", 15), width=10, command = "").place(x=1080, y=598)
-btnHuy = Button(f2, text="Hủy thao tác", fg=_primary, bg=_white, relief=FLAT,
-                font=("Segoe UI", 15), command = lambda: raise_frame(f1)).place(x=915, y=598)
-####
+btnHuy = Button(f2, text="Trở lại", fg=_primary, bg=_white, relief=FLAT,
+                 font=("Segoe UI", 15), command = lambda:raise_frame(f1)).place(x=1080, y=598)
 
 
-
+################################################################################################
+################################################################################################
+################################################################################################
 ###################################### F3: Hiển thị thông tin sau khi quét camera
 lblTitle = Label(f3, text='Hệ thống điểm danh bằng nhận diện khuôn mặt', font=(
                 "Segoe UI", 30, "bold"), bg=_primary, fg=_yellow).place(x=170, y=46)
@@ -321,33 +393,39 @@ lblBackground = Label(f3, text="", bg=_white, width=168,
 lblSubTitle = Label(f3, text="ĐIỂM DANH SINH VIÊN", font=(
                         "Segoe UI", 15, "bold"), fg=_primary, bg=_white).place(x=115, y=195)
 
-# lblRectangel = Label(f3, bg = _lightGray, width = 40, height = 18).place(x=319, y = 270)
+
 imgImage = Canvas(f3, bg=_white, width=245, height=245)
 imgImage.place(x=319, y=270)
-fileImgName = "hoa.png"
-imgSample = PhotoImage(file=fileImgName)
-imgImage.create_image(125, 125, anchor=CENTER, image=imgSample)
+fileName = "unknown3.png"
+imgSample = PhotoImage(file=fileName)
+imgOnCanvas = imgImage.create_image(125, 125, anchor=CENTER, image=imgSample)
 
 
-lblThongBao = Label(f3, text="ĐIỂM DANH THÀNH CÔNG", fg=_green, bg=_white, font=("Segoe UI", 15, "bold")).place(x=638, y=278)
+lblThongBao = Label(f3, text="ĐIỂM DANH THÀNH CÔNG", fg=_green, bg=_white, font=("Segoe UI", 15, "bold"))
+lblThongBao.place(x=638, y=278)
 
 lblMessage = Label(f3, text = "Sinh viên: Nguyễn Thị Thu Phương \n MSSV: 1151020028 \n Lớp: CNTT11-01",relief=SUNKEN,
-                             font=("Segoe UI", 15, "bold"), fg= _gray,bg = _white,height = 4, width= 30).place(x=638, y = 320)
+                             font=("Segoe UI", 15, "bold"), fg= _gray,bg = _white,height = 4, width= 30)
+lblMessage.place(x=638, y = 320)
 
 lblThoiGian = Label(f3, text="Thời gian check in:", font=("Segoe UI", 15, "bold"), 
-                                fg= _gray,bg = _white, height = 1).place(x= 638, y = 440)
+                                fg= _gray,bg = _white, height = 1)
+lblThoiGian.place(x= 638, y = 440)
 
-lblSetThoiGian = Label(f3, text = "14:00, 28/10/2020", fg=_primary, bg = _white, font=("Segoe UI", 15, "bold")).place(x=638, y=474)
+lblSetThoiGian = Label(f3, text = "14:00, 28/10/2020", fg=_primary, bg = _white, font=("Segoe UI", 15, "bold"))
+lblSetThoiGian.place(x=638, y=474)
 
 
 btnLuu = Button(f3, text="ĐIỂM DANH", fg=_white, bg=_lightBlue, 
-                font=("Segoe UI", 15), width=10, command = "").place(x=1080, y=598)
+                font=("Segoe UI", 15), width=10, command = TrackImages).place(x=1080, y=598)
 
 btnHuy = Button(f3, text="Trở lại trang chủ", fg=_primary, bg=_white, relief=FLAT,
-                font=("Segoe UI", 15), command = lambda: raise_frame(f1)).place(x=915, y=598)
+                font=("Segoe UI", 15), command = f3.destroy).place(x=915, y=598)
 
-
-raise_frame(f2)
+################################################################################################
+################################################################################################
+################################################################################################
+raise_frame(f1)
 root.mainloop()
 
 
